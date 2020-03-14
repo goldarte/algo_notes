2b98a2a79d75c1f6906aa25923405f810d3c26d7
Arthur Golubtsov <goldartt@gmail.com>
2020-03-14 18:14:10 +0300

    Make add note button action

--- notes.py
+++ notes.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QFileDialog, QApplication
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QApplication, QInputDialog, QLineEdit
 from notes_gui import Ui_MainWindow
 
 class MainWindow(QMainWindow):
@@ -10,7 +10,14 @@ class MainWindow(QMainWindow):
         self.init_ui()
 
     def init_ui(self):
-        pass
+        self.ui.addNoteButton.clicked.connect(self.add_note)
+
+    def add_note(self):
+        text, ok = QInputDialog.getText(self, "Добавить заметку",
+                                        "Название заметки: ")
+        if ok and text:
+            print(text)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
