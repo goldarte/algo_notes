92db6ffea55e95395a4532377ed2db404d5bb976
Arthur Golubtsov <goldartt@gmail.com>
2020-03-14 19:38:34 +0300

    Make add_note and remove_note functions

--- notes.py
+++ notes.py
@@ -1,6 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QFileDialog, QApplication, QInputDialog, QLineEdit
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QApplication, QInputDialog, QLineEdit, QMessageBox
 from notes_gui import Ui_MainWindow
+
+notes = {}
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -11,12 +13,32 @@ class MainWindow(QMainWindow):
 
     def init_ui(self):
         self.ui.addNoteButton.clicked.connect(self.add_note)
+        self.ui.removeNoteButton.clicked.connect(self.remove_note)
 
     def add_note(self):
-        text, ok = QInputDialog.getText(self, "Добавить заметку",
+        text, ok = QInputDialog().getText(self, "Добавить заметку",
                                         "Название заметки: ")
         if ok and text:
-            print(text)
+            if not text in notes.keys():
+                notes[text] = ""
+                self.ui.notesListWidget.addItem(text)
+                print(notes)
+            else:
+                error_msg = QMessageBox()
+                error_msg.setIcon(QMessageBox.Critical)
+                error_msg.setWindowTitle('Ошибка!')
+                error_msg.setText('Такая заметка уже есть! Введите другое имя.')
+                error_msg.exec_()
+
+    def remove_note(self):
+        if self.ui.notesListWidget.selectedItems():
+            current_item = self.ui.notesListWidget.currentItem()
+            index = self.ui.notesListWidget.row(current_item)
+            self.ui.notesListWidget.takeItem(index)
+            del notes[current_item.text()]
+            print(notes)
+        else:
+            print("Не выбран элемент!")
 
 
 if __name__ == "__main__":
